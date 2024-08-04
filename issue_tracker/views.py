3a3f9a1bb99ce356d31d3c1f13c37a08e23033d1
Thorbert1 <tobyas2005139@gmail.com>
2024-08-04 21:09:58 +0700

    Fixed naming conflict and fixed small logic error

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from .serializers import *
 
 from rest_framework.response import Response
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status as HTTPStatus
 from rest_framework.decorators import action
 
 
@@ -15,7 +15,7 @@ class PengaduanViewSet(viewsets.ModelViewSet):
         serializer = PengaduanSerializer(pengaduan)
 
         # If the user desires to stay anonymous, do not return user object
-        if pengaduan.anonymous :
+        if pengaduan.anonymous:
             pengaduan.user = None
             pengaduan.save()
 
@@ -24,33 +24,36 @@ class PengaduanViewSet(viewsets.ModelViewSet):
         # pengaduan.__setattr__("tanggal_post", unformatted_date.strftime("%Y-%m-%d, %X"))
 
         return Response(serializer.data)
-    
-    def update(self, request, pk=None) :
+
+    def update(self, request, pk=None):
         # User biasa : Judul, isi, lokasi (status = unresolved)
         # Admin : Status
         pengaduan = get_object_or_404(self.queryset, pk=pk)
         serializer = PengaduanSerializer(pengaduan)
 
-        if pengaduan.user.is_superuser :
-            status = request.data['status']
+        if pengaduan.user.is_superuser:
+            status = request.data["status"]
             pengaduan.status = status
             pengaduan.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        elif pengaduan.Status.UNRESOLVED :
-            judul = request.data['judul']
-            isi = request.data['isi']
-            lokasi = request.data['lokasi']
+            return Response(serializer.data, status=HTTPStatus.HTTP_200_OK)
+        elif pengaduan.status == Pengaduan.Status.UNRESOLVED:
+            judul = request.data["judul"]
+            isi = request.data["isi"]
+            lokasi = request.data["lokasi"]
 
             pengaduan.judul = judul
             pengaduan.isi = isi
             pengaduan.lokasi = lokasi
             pengaduan.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        return Response({'error_message' : 'Anda bukan Admin atau status bukan UNRESOLVED'}, status=status.HTTP_403_FORBIDDEN)
+            return Response(serializer.data, status=HTTPStatus.HTTP_200_OK)
 
-    def destroy(self, request, pk=None) :
-        # Requirement Delete Pengaduan: 
+        return Response(
+            {"error_message": "Anda bukan Admin atau status bukan UNRESOLVED"},
+            status=HTTPStatus.HTTP_403_FORBIDDEN,
+        )
+
+    def destroy(self, request, pk=None):
+        # Requirement Delete Pengaduan:
         # 1. Pengaduan harus milik user
         # 2. Status unresolved
         try:
@@ -59,80 +62,98 @@ class PengaduanViewSet(viewsets.ModelViewSet):
                 raise Exception("Anonymous tidak dapat menghapus pengaduan")
             elif pengaduan.user.id != request.data["user"]:
                 raise Exception("User tidak memiliki akses untuk menghapus pengaduan")
-            elif not pengaduan.Status.UNRESOLVED: 
+            elif not pengaduan.Status.UNRESOLVED:
                 raise Exception("Status bukan unresolved")
             else:
                 pengaduan.delete()
-                return Response(status=status.HTTP_200_OK)
+                return Response(status=HTTPStatus.HTTP_200_OK)
         except Exception as e:
-            return Response({'error_message' : f'{e}'}, status=status.HTTP_403_FORBIDDEN)
-        
-    @action(detail=False, methods=['get'], url_path='filter', url_name='filter')
+            return Response(
+                {"error_message": f"{e}"}, status=HTTPStatus.HTTP_403_FORBIDDEN
+            )
+
+    @action(detail=False, methods=["get"], url_path="filter", url_name="filter")
     # Hanya dapat melihat pengaduan yang dimiliki jika sudah login
     def filter(self, request):
         user = request.user.id
         pengaduan = Pengaduan.objects.filter(user=user)
         serializer = PengaduanSerializer(pengaduan, many=True)
         return Response(serializer.data)
-    
+
     def like(self, request, pk=None):
         pengaduan = get_object_or_404(self.queryset, pk=pk)
-        
+
         # Check if the like already exists to toggle like/unlike
         # Temporary implementation for 'akun_sso' field until authentication is done
-        pengaduan_like = Like.objects.get(akun_sso='akun_sso', pengaduan=pengaduan)
+        pengaduan_like = Like.objects.get(akun_sso="akun_sso", pengaduan=pengaduan)
         if pengaduan_like.exists():
             pengaduan_like.delete()
-            action = 'unliked'
+            action = "unliked"
         else:
-            Like.objects.create(akun_sso='akun_sso', pengaduan=pengaduan)
-            action = 'liked'
-        
+            Like.objects.create(akun_sso="akun_sso", pengaduan=pengaduan)
+            action = "liked"
+
         likes_count = Like.objects.filter(pengaduan=pengaduan).count()
-        
-        return Response({'amount_of_likes': likes_count, 'action': action}, status=status.HTTP_200_OK)
-    
-    def add_comment(self, request, pk=None) :
+
+        return Response(
+            {"amount_of_likes": likes_count, "action": action},
+            status=HTTPStatus.HTTP_200_OK,
+        )
+
+    def add_comment(self, request, pk=None):
         pengaduan = get_object_or_404(self.queryset, pk=pk)
 
         if not pengaduan.anonymous:
             author = request.user.username
-            isi = request.data.get('isi')
+            isi = request.data.get("isi")
 
             if isi:
                 comment = Comment(author=author, isi=isi, pengaduan=pengaduan)
                 comment.save()
                 comment_serializer = CommentSerializer(comment)
-                return Response(comment_serializer.data, status=status.HTTP_201_CREATED)
-            
-            return Response({'error_message': 'Komentar tidak boleh kosong!'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        return Response({'error_message' : 'User tidak terdaftar'}, status=status.HTTP_403_FORBIDDEN)
-    
-    def edit_comment(self, request, pk=None) :
+                return Response(
+                    comment_serializer.data, status=HTTPStatus.HTTP_201_CREATED
+                )
+
+            return Response(
+                {"error_message": "Komentar tidak boleh kosong!"},
+                status=HTTPStatus.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(
+            {"error_message": "User tidak terdaftar"},
+            status=HTTPStatus.HTTP_403_FORBIDDEN,
+        )
+
+    def edit_comment(self, request, pk=None):
         comment = get_object_or_404(Comment, pk=pk)
 
         if not comment.pengaduan.anonymous and comment.author == request.user.username:
-            isi = request.data.get('isi')
+            isi = request.data.get("isi")
             if isi:
                 comment.isi = isi
                 comment.save()
                 comment_serializer = CommentSerializer(comment)
-                return Response(comment_serializer.data, status=status.HTTP_200_OK)
-            
-            return Response({'error_message': 'Komentar tidak boleh kosong'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        return Response({'error_message' : 'User tidak terdaftar'}, status=status.HTTP_403_FORBIDDEN)
-    
-    def delete_comment(self, request, pk=None) :
+                return Response(comment_serializer.data, status=HTTPStatus.HTTP_200_OK)
+
+            return Response(
+                {"error_message": "Komentar tidak boleh kosong"},
+                status=HTTPStatus.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(
+            {"error_message": "User tidak terdaftar"},
+            status=HTTPStatus.HTTP_403_FORBIDDEN,
+        )
+
+    def delete_comment(self, request, pk=None):
         comment = get_object_or_404(Comment, pk=pk)
 
         if not comment.pengaduan.anonymous and comment.author == request.user.username:
             comment.delete()
-            return Response(status=status.HTTP_200_OK)
-        
-        return Response({'error_message' : 'User tidak terdaftar'}, status=status.HTTP_403_FORBIDDEN)
+            return Response(status=HTTPStatus.HTTP_200_OK)
 
-class UserViewSet(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
+        return Response(
+            {"error_message": "User tidak terdaftar"},
+            status=HTTPStatus.HTTP_403_FORBIDDEN,
+        )
