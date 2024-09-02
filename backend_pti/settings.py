789182b7f640559fe1b4c32570d6058e42b6f3b0
juankhusuma <juan.d.khusuma@gmail.com>
2024-09-03 00:49:37 +0700

    fix: allowed host fix

--- backend_pti/settings.py
+++ backend_pti/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["bem.cs.ui.ac.id", "https://bem.cs.ui.ac.id", "http://bem.cs.ui.ac.id"]
 
 
 # Application definition
