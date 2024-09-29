af04e99e4666f9bc2480d749c98a7e691dd3b1bc
juankhusuma <juan.d.khusuma@gmail.com>
2024-09-30 00:37:14 +0700

    backend cleanup

--- backend_pti/settings.py
+++ backend_pti/settings.py
@@ -114,6 +114,7 @@ REST_FRAMEWORK = {
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
