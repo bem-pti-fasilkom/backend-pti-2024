39462d5e8ad5f3089ac4207223fe579bd123fb6b
juankhusuma <juan.d.khusuma@gmail.com>
2024-11-24 00:51:34 +0700

    added whitenoise to requirements.txt

--- main_web/models.py
+++ main_web/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     img_url = models.CharField(max_length=255)
+    biro_name = models.CharField(max_length=255)
     event_name = models.CharField(max_length=255)
     start_date = models.DateTimeField()
-    registration_deadline = models.DateTimeField()
     end_date = models.DateTimeField()
     location = models.CharField(max_length=255)
     description = models.TextField()
