f9a10f89a8a6bb18aea518a4f81b37495fd9c42e
juankhusuma <juan.d.khusuma@gmail.com>
2024-11-24 01:09:18 +0700

    changed event biro name to multi select

--- main_web/models.py
+++ main_web/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Event(models.Model):
+    class Biro(models.TextChoices):
+        ADKESMA = "ADKESMA", _("Advokasi Kesejahteraan Mahasiswa")
+        BISMIT = "BISMIT", _("Bisnis dan Kemitraan")
+        HUMAS = "HUMAS", _("Hubungan Masyarakat")
+        KASTRAT = "KASTRAT", _("Kajian dan Aksi Strategis")
+        MEDIA = "MEDIA", _("Media")
+        DEPOR  = "DEPOR", _("Olahraga")
+        PENGMAS = "PENGMAS", _("Pengabdian Masyarakat")
+        PKKM = "PKKM", _("Pengembangan Karier dan Keilmuan Mahasiswa")
+        PSDM = "PSDM", _("Pengembangan Sumber Daya Manusia")
+        PTI = "PTI", _("Pengembangan Teknologi Informasi")
+        SENBUD = "SENBUD", _("Seni dan Budaya")
+
     img_url = models.CharField(max_length=255)
-    biro_name = models.CharField(max_length=255)
+    biro_name = models.CharField(max_length=255, choices=Biro.choices)
     event_name = models.CharField(max_length=255)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
