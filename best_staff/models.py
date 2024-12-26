d004e35bb26fc6b3a2f85309eafee81e82321cd5
StevenSetiawan <stevensetiawan1802@gmail.com>
2024-12-26 21:11:01 +0700

    feat: add model

--- best_staff/models.py
+++ best_staff/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class BEMMember(models.Model):
+    class Role(models.TextChoices):
+        STAFF = "STAFF"
+        BPH = "BPH"
+        KOOR = "KOOR"
+    
+    role = models.CharField(
+        max_length = 5, choices = Role.choices, default = Role.STAFF
+    )
+    
+    img_url = models.URLField()
+
+class Event(models.Model):
+    start = models.DateTimeField(editable=True, default=timezone.now)
+    end = models.DateTimeField(editable=True)
