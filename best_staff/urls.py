637987ea754f2dce7f8341619951e795c7830bc4
Helvennn <enixgregory@gmail.com>
2025-11-08 19:53:36 +0700

    fix: API docs url position changes

--- best_staff/urls.py
+++ best_staff/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 from .views import VoteAPIView
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-
 urlpatterns = [
     path('authenticate/', views.authenticate_staff, name='authenticate'),
     path('event/', views.get_event, name='event'),
@@ -12,10 +10,4 @@ urlpatterns = [
     path('vote/<str:voted_npm>/', VoteAPIView.as_view(), name='vote'),
     path('vote/statistics/<str:birdept>/', VoteAPIView.as_view(), name='vote_statistics'),
     path('statistics/', views.get_all_statistics, name='all_vote_statistics'),
-
-    # Endpoint buat API documentation
-    path('docs/', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
 ]
