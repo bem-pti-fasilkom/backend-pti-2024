7929632e04e47223ed6a57a7517b6520f76dc5ce
Benhanan14406 <a.b.abinaya.fuun@gmail.com>
2025-11-29 21:15:08 +0700

    Forgot to delete debugging delete all and adjusted the url names

--- cloudinary_app/urls.py
+++ cloudinary_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("image/", CloudinaryImageGetCreate.as_view(), name = "uploadImage"),
-    path("image/<uuid:id>/", CloudinaryImageGetCreate.as_view(), name = "uploadImage"),
-    path("video/", CloudinaryVideoGetCreate.as_view(), name = "displayImages"),
-    path("video/<uuid:id>/", CloudinaryVideoGetCreate.as_view(), name = "displayImages"),
+    path("image/", CloudinaryImageGetCreate.as_view(), name = "uploadNgetAllImages"),
+    path("image/<uuid:id>/", CloudinaryImageGetCreate.as_view(), name = "getImageById"),
+    path("video/", CloudinaryVideoGetCreate.as_view(), name = "uploadNgetAllVideos"),
+    path("video/<uuid:id>/", CloudinaryVideoGetCreate.as_view(), name = "getVideoById"),
 ]
